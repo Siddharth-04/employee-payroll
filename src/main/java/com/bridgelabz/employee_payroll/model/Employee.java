95d61360ad437ea9ac37893f22fa61265c2b8071
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-17 19:09:36 +0530

    [Siddharth] ADD: Added Lombok dependency and removed getter setter methods from Employee model

--- src/main/java/com/bridgelabz/employee_payroll/model/Employee.java
+++ src/main/java/com/bridgelabz/employee_payroll/model/Employee.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,7 +17,7 @@ public class Employee {
     private String name;
     private long salary;
 
-    public Employee( String department, String name, long salary) {
+    public Employee(String department, String name, long salary) {
         this.department = department;
         this.name = name;
         this.salary = salary;
@@ -23,37 +25,5 @@ public class Employee {
 
     public Employee() {
 
-    }
-
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
     }
 }
