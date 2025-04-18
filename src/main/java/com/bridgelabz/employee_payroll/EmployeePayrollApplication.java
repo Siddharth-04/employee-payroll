fa3775234c6ae529b9f898dcf08156d1760dd2f9
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-18 18:27:58 +0530

    [Siddharth] ADD: Added logging in this application for debugging

--- src/main/java/com/bridgelabz/employee_payroll/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employee_payroll/EmployeePayrollApplication.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.employee_payroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Application started ");
 	}
-
 }
--- src/main/java/com/bridgelabz/employee_payroll/service/EmployeeServiceImpl.java
+++ src/main/java/com/bridgelabz/employee_payroll/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.bridgelabz.employee_payroll.dto.EmployeeDTO;
 import com.bridgelabz.employee_payroll.dto.ResponseDTO;
 import com.bridgelabz.employee_payroll.model.Employee;
 import com.bridgelabz.employee_payroll.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class EmployeeServiceImpl implements IEmployeeService{
 
     @Autowired
@@ -21,6 +23,7 @@ public class EmployeeServiceImpl implements IEmployeeService{
     public ResponseEntity<Employee> createEmployee(EmployeeDTO employee) {
         Employee newEmployee = new Employee(employee);
         employeeRepository.save(newEmployee);
+        log.info("Employee created with id: {}", newEmployee.getEmployeeId());
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 
@@ -30,6 +33,7 @@ public class EmployeeServiceImpl implements IEmployeeService{
 
         if(obj.isPresent()){
             Employee emp = obj.get();
+            log.info("Employee found with id: {}", emp.getEmployeeId());
             return new ResponseEntity<>(emp,HttpStatus.OK);
         }
 
@@ -50,12 +54,12 @@ public class EmployeeServiceImpl implements IEmployeeService{
             if (employee.getNote() != null) em.setNote(employee.getNote());
             if (employee.getProfilePic() != null) em.setProfilePic(employee.getProfilePic());
             if (employee.getStartDate() != null) em.setStartDate(employee.getStartDate());
-
+            log.info("Employee updated with id: {}", em.getEmployeeId());
             employeeRepository.save(em);
             return new ResponseDTO("User details updated",HttpStatus.OK);
         }
 
-
+        log.error("Employee not found with id: {}", id);
         return new ResponseDTO("User not found with this ID",HttpStatus.NOT_FOUND);
     }
 
@@ -64,10 +68,12 @@ public class EmployeeServiceImpl implements IEmployeeService{
         Optional<Employee> obj = employeeRepository.findById(id);
 
         if(obj.isPresent()){
+            log.info("Employee deleted with id: {}", id);
             employeeRepository.deleteById(id);
             return new ResponseEntity<>("User deleted with id "+id,HttpStatus.NO_CONTENT);
         }
 
+        log.error("Employee not found : {}", id);
         return new ResponseEntity<>("User not found with id "+id,HttpStatus.NOT_FOUND);
     }
 
